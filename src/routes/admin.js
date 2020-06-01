70af0407c781260171fbd01b7a728947b4794b01
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-06-01 17:31:59 +0100

    fix: remove src from master

--- src/routes/admin.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const adminController = require("../controllers/admin");
-const router = Router();
-
-router.post("/login", adminController.login);
-
-module.exports = router;
--- src/utils/hash_password.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10);
-};
-
-module.exports = hashPassword;
